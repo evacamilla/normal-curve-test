92611eb083cb7aa16c91b46e311eae078be4eced
wongprom <jimmybackstrom@hotmail.com>
2018-09-20 16:02:58 +0200

    Show different content depending on state.

--- src/components/Introduction.js
+++ src/components/Introduction.js
@@ -1,16 +1,42 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-function Introduction(){
-    return(
-        <div>
+class Introduction extends Component {
+  state = {
+    showWelcome: true
+  };
+  render() {
+    return (
+      <div>
+        {this.state.showWelcome ? (
+          <div>
             <h1>Välkommen!</h1>
-
-            <p>Här är en kortare text om vad själva kursen går ut på.<br />
-            (Vem den riktar sig till?)<br />
-            Eventuellt en illustration för stämningens skull, passar målgruppen eller ej?<br />
+            <p>
+              Här är en kortare text om vad själva kursen går ut på.
+              <br />
+              (Vem den riktar sig till?)
+              <br />
+              Eventuellt en illustration för stämningens skull, passar
+              målgruppen eller ej?
+              <br />
             </p>
-        </div>
+          </div>
+        ) : (
+          <div>
+            <h1>Steg 1</h1>
+            <p>
+              Instruktioner om steg 1. <br />- användaren ska skriva ut pdf{' '}
+              <br />
+              video kommer visas <br />
+              användaren ska under tiden fylla i frågorna i sitt papper.
+              <br />
+              Skriv ut: En bild på pdf med textad länk Texten kan "flyta" med
+              bilden
+            </p>
+          </div>
+        )}
+      </div>
     );
+  }
 }
 
 export default Introduction;
--- src/components/Question.js
+++ src/components/Question.js
@@ -1,19 +1,21 @@
 import React, { Component } from 'react';
 
 class Question extends Component {
-    state = {
-    }
-    render(){
-        return(
-            <div>
-                <h1>Nedstämdhet</h1>
-                <p>
-                    Avser en sänkning av det emotionella grundläget (till skillnad från situationsutlösta affekter). Omfattar dysterhet, tungsinne och nedstämdhet, som manifesterar sig i mimik, kroppshållning och rörelsemönster. Bedömningen baseras på utpräglingsgrad och avledbarhet.
-                    Förhöjd grundstämning.
-                </p>
-            </div>
-        );
-    }
+  state = {};
+  render() {
+    return (
+      <div>
+        <h1>Nedstämdhet</h1>
+        <p>
+          Avser en sänkning av det emotionella grundläget (till skillnad från
+          situationsutlösta affekter). Omfattar dysterhet, tungsinne och
+          nedstämdhet, som manifesterar sig i mimik, kroppshållning och
+          rörelsemönster. Bedömningen baseras på utpräglingsgrad och
+          avledbarhet. Förhöjd grundstämning.
+        </p>
+      </div>
+    );
+  }
 }
 
 export default Question;
