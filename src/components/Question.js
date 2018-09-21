b64a0447f145a3465c5864b569200f8e38938ded
wongprom <jimmybackstrom@hotmail.com>
2018-09-21 11:02:23 +0200

    Create btn component and added to Result, question,video and introduction component

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
-
+import BtnChangePage from './BtnChangePage.js';
 class Question extends Component {
   state = {
     chapter: 'Resultat'
   };
-  
+
   componentWillMount = () => {
     this.props.changeChapter(this.state.chapter);
-  }
+  };
 
   render() {
     return (
@@ -20,6 +20,7 @@ class Question extends Component {
           rörelsemönster. Bedömningen baseras på utpräglingsgrad och
           avledbarhet. Förhöjd grundstämning.
         </p>
+        <BtnChangePage />
       </div>
     );
   }
--- src/components/Result.js
+++ src/components/Result.js
@@ -1,25 +1,21 @@
 import React, { Component } from 'react';
+import BtnChangePage from './BtnChangePage.js';
 
 class Result extends Component {
   state = {
     chapter: 'Resultat'
   };
-  
+
   componentDidMount = () => {
     this.props.changeChapter(this.state.chapter);
-  }
-  
+  };
+
   render() {
     return (
       <div>
-        <h1>Nedstämdhet</h1>
-        <p>
-          Avser en sänkning av det emotionella grundläget (till skillnad från
-          situationsutlösta affekter). Omfattar dysterhet, tungsinne och
-          nedstämdhet, som manifesterar sig i mimik, kroppshållning och
-          rörelsemönster. Bedömningen baseras på utpräglingsgrad och
-          avledbarhet. Förhöjd grundstämning.
-        </p>
+        <h1>Resultat</h1>
+        <p>Du är godgänd</p>
+        <BtnChangePage />
       </div>
     );
   }
