fc7ef17bbabcaaed81a23ad1b54a842d541bbad0
wongprom <jimmybackstrom@hotmail.com>
2018-09-21 15:10:23 +0200

    Test different scenarios in result component.

--- src/components/Result.js
+++ src/components/Result.js
@@ -4,8 +4,8 @@ import Button from './Button.js';
 class Result extends Component {
   state = {
     chapter: 'Resultat',
-    filledInAnswers: true,
-    passedTest: true
+    filledInAnswers: false,
+    passedTest: false
   };
 
   componentDidMount = () => {
@@ -14,39 +14,46 @@ class Result extends Component {
 
   answersOverview = () => {
     console.log('answers overview');
-  }
-
-
+  };
 
   render() {
     return (
       <div>
         <h1>Resultat</h1>
-        
-        { this.state.filledInAnswers ? 
-        //if user filled out all the answers
-        <div>
-          { this.state.passedTest ? 
-          //if user did pass the test
+
+        {this.state.filledInAnswers ? (
+          //if user filled out all the answers
           <div>
-            <p>Du är godkänd</p> 
-            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/8c/Standard_deviation_diagram.svg/400px-Standard_deviation_diagram.svg.png" />
-            <Button text={'Se överblick'} onClick={this.answersOverview} className={'big-btn'} />
-          </div> 
-          : 
-          //if user did not pass
-          <div> 
-            <p>Du är inte godkänd</p> 
-            <Button text={'Gör om kursen'} onClick={this.answersOverview} className={'big-btn'} />
+            {this.state.passedTest ? (
+              //if user did pass the test
+              <div>
+                <p>Du är godkänd</p>
+                <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/8c/Standard_deviation_diagram.svg/400px-Standard_deviation_diagram.svg.png" />
+                <Button
+                  text={'Se överblick'}
+                  onClick={this.answersOverview}
+                  className={'big-btn'}
+                />
+              </div>
+            ) : (
+              //if user did not pass
+              <div>
+                <p>Du är inte godkänd</p>
+                <Button
+                  text={'Gör om kursen'}
+                  onClick={this.answersOverview}
+                  className={'big-btn'}
+                />
+              </div>
+            )}
           </div>
-        }
-        </div>
-          : //if user didnt fill out all the answers(came in to page with URL, deny access?)
-        <div>
-          Du måste göra klart kursen för att kunna se dina resultat
-          <Button text={'ggew'} onClick={this.answersOverview} />
-        </div>
-      }
+        ) : (
+          //if user didnt fill out all the answers(came in to page with URL, deny access?)
+          <div>
+            Du måste göra klart kursen för att kunna se dina resultat
+            <Button text={'ggew'} onClick={this.answersOverview} />
+          </div>
+        )}
       </div>
     );
   }
