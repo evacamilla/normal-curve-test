932d30f82a81fc01a08215bf26ba6bd50e57069a
wongprom <jimmybackstrom@hotmail.com>
2018-09-24 10:56:15 +0200

    Add dumyDB

--- src/components/Question.js
+++ src/components/Question.js
@@ -2,39 +2,46 @@ import React, { Component } from 'react';
 import BtnChangePage from './BtnChangePage.js';
 class Question extends Component {
   state = {
-    chapter: 'Fyll i dina svar'
+    chapter: 'Fyll i dina svar',
+    heading: allaFragor[0].heading,
+    question: allaFragor[0].question,
+    questionIndex: 0
   };
 
-  componentWillMount = () => {
-    this.props.changeChapter(this.state.chapter);
+  //componentDidMount = () => {
+  //this.props.changeChapter(this.state.chapter);
+  //};
+
+  showOneQuestion = index => {
+    this.setState({ heading: allaFragor[index].heading });
+  };
+
+  showNextQuestion = () => {
+    let i = this.state.questionIndex;
+    this.setState({ heading: allaFragor[i].heading });
+    this.setState({ questionIndex: this.state.questionIndex + 1 });
   };
 
   render() {
-    allafragor = [
-      {
-        heading: 'Nedstämdhet',
-        question:
-          'Avser uppgift om sänkt grundstämning oavsett om den tar sig uttryck eller ej.  Omfattar känsloravsorgsenhet,olycklighet,hopplöshetochhjälplöshet.  Bedömningenbaseraspåintensitet,varaktighetochi  vilkengradsinnesstäm-ningenpåverkasavyttreomständigheter.Förhöjdsinnesstämningskattas”0”'
-      },
-      {
-        heading: 'Sänkt grundstämming',
-        question: 'Avser en sänkning av det emotionella... '
-      }
-    ];
-
     return (
       <div>
-        <h1>{allafragor[0]}</h1>
-        <p>
-          Avser en sänkning av det emotionella grundläget (till skillnad från
-          situationsutlösta affekter). Omfattar dysterhet, tungsinne och
-          nedstämdhet, som manifesterar sig i mimik, kroppshållning och
-          rörelsemönster. Bedömningen baseras på utpräglingsgrad och
-          avledbarhet. Förhöjd grundstämning.
-        </p>
+        <h1>{this.state.heading}</h1>
+        <p>{this.state.question}</p>
+        <button onClick={this.showNextQuestion}>Nästa</button>
       </div>
     );
   }
 }
 
+let allaFragor = [
+  {
+    heading: 'Nedstämdhet',
+    question:
+      'Avser uppgift om sänkt grundstämning oavsett om den tar sig uttryck eller ej.  Omfattar känsloravsorgsenhet,olycklighet,hopplöshetochhjälplöshet.  Bedömningenbaseraspåintensitet,varaktighetochi  vilkengradsinnesstäm-ningenpåverkasavyttreomständigheter.Förhöjdsinnesstämningskattas”0”'
+  },
+  {
+    heading: 'Sänkt grundstämming',
+    question: 'Avser en sänkning av det emotionella... '
+  }
+];
 export default Question;
