a3a4d11823899cb258ebf77e4ca7f5435f0e5148
cami <evacamilla.s@gmail.com>
2018-11-08 11:25:55 +0100

    Start on component for displaying answer alternatives in Question.js

--- src/components/Question.js
+++ src/components/Question.js
@@ -3,6 +3,7 @@ import Button from './Button.js';
 import { Link } from 'react-router-dom';
 import DisplayAllQuickQuestion from './DisplayAllQuickQuestion';
 import OneQuestion from './OneQuestion';
+import AnswerAlternative from './AnswerAlternative';
 
 class Question extends Component {
   state = {
@@ -57,27 +58,13 @@ class Question extends Component {
 
   //question loop
   displayAnswerAlternatives = () => {
-    console.log('dispayone' + this.state.oneQuestion);
     let answersUl = [];
     let index = 0;
-    let answers = this.props.allaFragor[this.state.questionIndex].answers;
 
     //gör om till map?
-    for (let answer of answers) {
+    for (let answer of this.props.allaFragor[this.state.questionIndex].answers) {
       let li = (
-        <li
-          className="question-point-li"
-          key={index}
-          id={index}
-          onClick={this.temporaryAnswer}
-        >
-          <div id={index} className="question-point">
-            {index}
-          </div>
-          <div id={index} className="question-answers-text">
-            <p id={index}>{answer}</p>
-          </div>
-        </li>
+        <AnswerAlternative index={index} temporaryAnswer={this.temporaryAnswer} answer={answer}/>
       );
       answersUl.push(li);
       index += 1;
