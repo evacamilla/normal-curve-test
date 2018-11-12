0b0afaadec50dbc42188aa5b9ee7efc28bee1b10
cami <evacamilla.s@gmail.com>
2018-11-12 11:36:04 +0100

    Remove setQuestion from the question div that is already showingn.

--- src/components/QuestionAccordion.js
+++ src/components/QuestionAccordion.js
@@ -4,14 +4,13 @@ import AnswerAlternative from './AnswerAlternative.js';
 function QuestionAccordion(props) {
   if(props.questionIndex == props.id ){
     return (  
-      <div onClick={props.handleClick} id={props.id} className="accordion">
-        <h1 id={props.id}>{props.number + " " + props.heading}</h1>
+      <div className="accordion">
+        <h1 id={props.id}>{props.number + ". " + props.heading}</h1>
+
         <div className="one-question">
           <p>
             {props.question}
           </p>
-        </div>
-
         <div className="question-answers">
           <ul>
             {props.allaFragor[
@@ -29,11 +28,12 @@ function QuestionAccordion(props) {
           </ul>
           </div>
       </div>
+        </div>
   );
   } else {
     return (  
       <div onClick={props.handleClick} id={props.id} className="accordion">
-        <h1 id={props.id}>{props.number + " " + props.heading}</h1>
+        <h1 id={props.id}>{props.number + ". " + props.heading}</h1>
       </div>
     );
   }
